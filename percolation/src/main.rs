3e05f989557f5fb5f42287e3f226cd5b6e402346
Troy Caro <iamtroy412@gmail.com>
2023-03-17 19:34:19 -0400

    Percolation constructor

--- percolation/src/main.rs
+++ percolation/src/main.rs
@@ -1,7 +1,17 @@
 use percolation::UnionFind;
 
-struct Percolation {
+pub struct Percolation {
     n: usize,
     uf: UnionFind,
     opened: Vec<bool>,
+}
+
+impl Percolation {
+    pub fn new(n: usize) -> Self {
+        Percolation {
+            n,
+            uf: UnionFind::new(n * n),
+            opened: vec![false; n * n],
+        }
+    }
 }
