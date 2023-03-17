29126be90afab5c3b75b8ee6f03b9c6047b308dc
Troy Caro <iamtroy412@gmail.com>
2023-03-17 19:42:58 -0400

    Define fn headers for Percolation impl

--- percolation/src/main.rs
+++ percolation/src/main.rs
@@ -14,4 +14,15 @@ impl Percolation {
             opened: vec![false; n * n],
         }
     }
+
+    pub fn open(&mut self, row: usize, col: usize) {}
+
+    pub fn is_open(&self, row: usize, col: usize) -> bool {}
+
+    pub fn is_full(&self, row: usize, col: usize) -> bool {}
+
+    pub fn num_open_sites(&self) -> usize {}
+
+    // Does the system percolate?
+    pub fn percolates(&self) -> bool {}
 }
