b73b534843c29340ef34846899a38ade65f18505
Troy Caro <iamtroy412@gmail.com>
2023-03-17 20:02:49 -0400

    Determine index of 1d array from row, col, based on the N-by-N size of our grid. Also implemented is_open and num_open_sites

--- percolation/src/main.rs
+++ percolation/src/main.rs
@@ -16,17 +16,25 @@ impl Percolation {
         }
     }
 
+    fn index_of(&self, row: usize, col: usize) -> usize {
+        (row - 1) * self.n + col - 1
+    }
+
     // opens the site (row, col) if it is not already open
     pub fn open(&mut self, row: usize, col: usize) {}
 
     // is the site (row, col) open?
-    pub fn is_open(&self, row: usize, col: usize) -> bool {}
+    pub fn is_open(&self, row: usize, col: usize) -> bool {
+        self.opened[self.index_of(row, col)]
+    }
 
     // is the site (row, col) full?
     pub fn is_full(&self, row: usize, col: usize) -> bool {}
 
     // returns the number of open sites
-    pub fn num_open_sites(&self) -> usize {}
+    pub fn num_open_sites(&self) -> usize {
+        self.opened.iter().filter(|&x| *x == true).count()
+    }
 
     // Does the system percolate?
     pub fn percolates(&self) -> bool {}
