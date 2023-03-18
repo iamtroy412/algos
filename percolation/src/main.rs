ae662a5e48a4bf19c05bdd759f058c8c0ae450bd
Troy Caro <iamtroy412@gmail.com>
2023-03-18 17:02:26 -0400

    Add clap cli parser and required args for size and number of experiments

--- percolation/src/main.rs
+++ percolation/src/main.rs
@@ -1,9 +1,21 @@
+use clap::Parser;
 use percolation::stats::PercolationStats;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Dimension of grid to create (SIZE * SIZE)
+    size: usize,
+
+    /// Number of experiments to run
+    exp: usize,
+}
+
 fn main() {
+    let cli = Cli::parse();
     println!("Hello!");
 
-    let stats = PercolationStats::new(200, 100);
+    let stats = PercolationStats::new(cli.size, cli.exp);
     println!("{:23} = {}", "mean", stats.mean());
     println!("{:23} = {}", "stddev", stats.std_dev());
     println!(
