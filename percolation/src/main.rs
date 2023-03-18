283896817ff6bc8dbfa0c0c768b5a28d906e2879
Troy Caro <iamtroy412@gmail.com>
2023-03-18 17:37:59 -0400

    Validate that the number of experiments command argument is greater than 30 before starting the program

--- percolation/src/main.rs
+++ percolation/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use percolation::stats::PercolationStats;
+use std::ops::RangeFrom;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -8,7 +9,24 @@ struct Cli {
     size: usize,
 
     /// Number of experiments to run
+    #[arg(value_parser = exp_in_range)]
     exp: usize,
+}
+
+const MIN_EXP: RangeFrom<usize> = 30..;
+
+fn exp_in_range(s: &str) -> Result<usize, String> {
+    let exp: usize = s
+        .parse()
+        .map_err(|_| format!("`{s}` isn't a valid number"))?;
+    if MIN_EXP.contains(&exp) {
+        Ok(exp)
+    } else {
+        Err(format!(
+            "Number of experiments needs to be greater than {}!",
+            MIN_EXP.start
+        ))
+    }
 }
 
 fn main() {
