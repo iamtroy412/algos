63360e1051f36e42e34292157d5d192b8b4c0520
Troy Caro <iamtroy412@gmail.com>
2023-03-18 15:35:31 -0400

    init main - first test simulation

--- percolation/src/main.rs
+++ percolation/src/main.rs
@@ -71,4 +71,14 @@ impl PercolationStats {
 
 fn main() {
     println!("Hello!");
+
+    let stats = PercolationStats::new(200, 100);
+    println!("{:23} = {}", "mean", stats.mean());
+    println!("{:23} = {}", "stddev", stats.std_dev());
+    println!(
+        "{:23} = {}, {}",
+        "95% confidence interval",
+        stats.confidence_low(),
+        stats.confidence_high()
+    );
 }
