069daa63cc3508889d3b57fb14788f70b0c35125
Troy Caro <iamtroy412@gmail.com>
2023-03-21 19:52:41 -0400

    Add item to the front of the deque

--- permutation/src/deque.rs
+++ permutation/src/deque.rs
@@ -1,31 +1,34 @@
-use std::ptr::NonNull;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
 struct Node<T> {
     val: T,
-    next: Option<NonNull<Node<T>>>,
-    prev: Option<NonNull<Node<T>>>,
+    next: Link<T>,
+    prev: Link<T>,
 }
 
 impl<T> Node<T> {
-    fn new(t: T) -> Node<T> {
-        Node {
+    fn new(t: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
             val: t,
             next: None,
             prev: None,
-        }
+        }))
     }
 }
 
 pub struct Deque<T> {
     length: usize,
-    head: Option<NonNull<Node<T>>>,
-    tail: Option<NonNull<Node<T>>>,
+    head: Link<T>,
+    tail: Link<T>,
 }
 
 impl<T> Deque<T> {
     // construct an empty deque
     pub fn new() -> Self {
-        Self {
+        Deque {
             length: 0,
             head: None,
             tail: None,
@@ -41,6 +44,24 @@ impl<T> Deque<T> {
     // return the number of items on the deque
     pub fn size(&self) -> usize {
         self.length
+    }
+
+    // add the item to the front
+    pub fn add_frist(&mut self, item: T) {
+        let new_head = Node::new(item);
+
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+        self.length += 1;
     }
 }
 
