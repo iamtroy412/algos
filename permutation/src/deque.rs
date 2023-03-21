9678fd621e5cd0d1b01a4f7c8227aa01f9cbd0af
Troy Caro <iamtroy412@gmail.com>
2023-03-21 19:11:08 -0400

    get the size of the deque and if it's empty or not

--- permutation/src/deque.rs
+++ permutation/src/deque.rs
@@ -23,12 +23,24 @@ pub struct Deque<T> {
 }
 
 impl<T> Deque<T> {
+    // construct an empty deque
     pub fn new() -> Self {
         Self {
             length: 0,
             head: None,
             tail: None,
         }
+    }
+
+    // is the deque empty?
+    pub fn is_empty(&self) -> bool {
+        // self.head == None
+        self.length == 0
+    }
+
+    // return the number of items on the deque
+    pub fn size(&self) -> usize {
+        self.length
     }
 }
 
