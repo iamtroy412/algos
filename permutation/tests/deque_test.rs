14f505012d3fa8f5539d6e88f406391ca78c3177
Troy Caro <iamtroy412@gmail.com>
2023-03-23 10:21:45 -0400

    First quick tests for adding first/last and removing first

--- permutation/tests/deque_test.rs
+++ permutation/tests/deque_test.rs
@@ -1,8 +1,48 @@
 use permutation::deque::*;
 
 #[test]
-fn new_deque() {
+fn empty_deque_test() {
     let empty_deque = Deque::<i32>::new();
     assert!(empty_deque.is_empty());
     assert_eq!(0, empty_deque.size());
+}
+
+#[test]
+fn add_first_test() {
+    let mut deque = Deque::<i32>::new();
+
+    for i in 0..10 {
+        deque.add_frist(i);
+    }
+
+    assert_eq!(false, deque.is_empty());
+    assert_eq!(10, deque.size());
+}
+
+#[test]
+fn add_last_test() {
+    let mut deque = Deque::<i32>::new();
+
+    for i in 0..20 {
+        deque.add_last(i);
+    }
+
+    assert_eq!(false, deque.is_empty());
+    assert_eq!(20, deque.size());
+}
+
+#[test]
+fn remove_first_test() {
+    let mut deque = Deque::<i32>::new();
+
+    // removing from an empty deque should return `None`
+    assert_eq!(None, deque.remove_first());
+
+    for i in 0..10 {
+        deque.add_frist(i);
+    }
+
+    for i in 9..=0 {
+        assert_eq!(i, deque.remove_first().unwrap());
+    }
 }
